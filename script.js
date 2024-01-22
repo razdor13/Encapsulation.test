25c3b69a29becfc87eafa885fca29a0ea5302467
Razdor13 <oneamaday@gmail.com>
2024-01-22 18:03:17 +0200

    done

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+
+    #surname = 'Tsymmer';
+
+    say = () => {
+        console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
+}
+
+const ivan = new User('Valentyn', 27);
